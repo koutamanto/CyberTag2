21b7e41cd9ade3686b9fba466ba6096dbbde4474
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-20 22:18:32 +0900

    a

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
-
-app.use(express.static(__dirname + "/"))
-
-var server = http.createServer(app)
-server.listen(port)
-
-console.log("http server listening on %d", port)
-
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io');
+const cron = require('node-cron');
+ 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+ 
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+ 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+ 
+cron.schedule('*/5 * * * * *', () => {
+  console.log('send hello.');
+  io.emit('message', 'hello');
+});
