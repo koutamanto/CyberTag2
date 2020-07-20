f50e01ae12f01eb687ca87e1657eb3308b006cf3
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-20 22:19:35 +0900

    no message

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io');
-const cron = require('node-cron');
  
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -17,9 +16,4 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-});
- 
-cron.schedule('*/5 * * * * *', () => {
-  console.log('send hello.');
-  io.emit('message', 'hello');
 });
