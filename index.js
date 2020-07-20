9ba674e00ce4753c1421adee6efdffe4b243e386
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-20 22:22:56 +0900

    no message

--- index.js
+++ index.js
@@ -2,18 +2,5 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io');
- 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
- 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
- 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
+io.listen(http);
+http.listen(8080);
