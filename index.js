86012c7550e28efc30299ca8d207e15652fb10d7
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-20 22:24:48 +0900

    no message

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var express = require('express');
+var router = express.Router();
+
+/* 以下追加 */
 var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io');
-io.listen(http);
-http.listen(8080);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+/* 以上追加 */
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* 以下追加 */
+io.on('connection', function(socket) {
+  console.log('connected!!!');
+});
+/* 以上追加 */
+
+module.exports = router;
