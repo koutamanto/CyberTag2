929a1fd7f25b7f0211ca0e54e9ed78fcfb1fd1bd
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-23 13:02:46 +0900

    no message

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
       <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
   <body>
+  <body id="page-top">
+        <!--Google map-->
+        <main class="container">
+            <div id="map" class="map"></div>
+            <p id="info" class="info"></p>
+          </main>
     <script type="text/javascript">
     const createMap = ({ lat, lng }) => {
   return new google.maps.Map(document.getElementById('map'), {
@@ -312,5 +318,15 @@ $.get("https://cybertagapi.herokuapp.com/getLocation",
   $info.classList.remove('error');
 };
     </script>
+      <!--Google Maps-->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Contact form JS-->
+        <script src="assets/mail/jqBootstrapValidation.js"></script>
+        <script src="assets/mail/contact_me.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
   </body>
 </html>
