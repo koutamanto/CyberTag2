292baa248ea3a8a865c23e888a17a06e0d0c20d4
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-23 13:16:56 +0900

    no message

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 </head>
   <body>
-  <body id="page-top">
+  <body>
         <!--Google map-->
         <main class="container">
             <div id="map" class="map"></div>
