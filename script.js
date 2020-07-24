ffacd224aa6393060a5f6dd69074b28d32ac81c0
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-24 14:50:15 +0900

    no message

--- script.js
+++ script.js
@@ -286,29 +286,28 @@ var countup = function(){
   var xhr = new XMLHttpRequest();
   xhr.open('GET','https://cybertagapi.herokuapp.com:27349/getLocation');
   xhr.onreadystatechange = function(){
-
     if(this.readyState == 4 && this.status == 200){
         data = this.responseText;
-        console.log(user);
+        console.log(data);
+        console.log(JSON.parse(data))
+        var lats = data.lat;
+        console.log(lats)
+        var lat = parseFloat(lats);
+        console.log(lat)
+        var lngs = data.lng;
+        console.log(lngs)
+        var lng = parseFloat(lngs);
+        console.log(lng)
+        const initialPosition = { lat: lat, lng: lng };
+        const map = createMap(initialPosition);
+        const marker = createMarker({ map, position: initialPosition });
+        const $info = document.getElementById('info');
+        marker.setPosition({ lat, lng });
+        map.panTo({ lat, lng });
+        $info.textContent = `Lat: ${lat.toFixed(5)} Lng: ${lng.toFixed(5)}`;
+        $info.classList.remove('error');
     }
 }
-  console.log(JSON.parse(data))
-  var lats = data.lat;
-  console.log(lats)
-  var lat = parseFloat(lats);
-  console.log(lat)
-  var lngs = data.lng;
-  console.log(lngs)
-  var lng = parseFloat(lngs);
-  console.log(lng)
-  const initialPosition = { lat: lat, lng: lng };
-  const map = createMap(initialPosition);
-  const marker = createMarker({ map, position: initialPosition });
-  const $info = document.getElementById('info');
-  marker.setPosition({ lat, lng });
-  map.panTo({ lat, lng });
-  $info.textContent = `Lat: ${lat.toFixed(5)} Lng: ${lng.toFixed(5)}`;
-  $info.classList.remove('error');
 }
 /**
  * Initialize the application.
