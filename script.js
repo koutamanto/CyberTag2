8adf1d89055fbc8a0064c9e28b1479fcea42dc3c
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-24 14:41:22 +0900

    no message

--- script.js
+++ script.js
@@ -285,7 +285,7 @@ const getPositionErrorMessage = code => {
 var countup = function(){
   var xhr = new XMLHttpRequest();
   xhr.open('GET','https://cybertagapi.herokuapp.com:27349/getLocation');
-  var data = xhr.JSON.parse(xhr.responseText);
+  var data = xhr.responseText;
   console.log(data)
   var lats = data["lat"];
   console.log(lats)
